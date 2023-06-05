851633e7d8adc1bec0b4a9b13d63b5ed44474592
Nur <nuriswanto66@gmail.com>
2023-06-05 13:40:47 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,10 @@ import CreateUserForm from '@/components/users/CreateUserForm';
 // reference use client
 // https://stackoverflow.com/questions/74965849/youre-importing-a-component-that-needs-usestate-it-only-works-in-a-client-comp
 
+// npm install mongodb
+
+// npm install react-query
+
 const Home: React.FC = () => {
   return (
     <div>
